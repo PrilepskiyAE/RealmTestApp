9603fd81f3953518f8f22bd8fbb34f4e7ec8b578
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-05-09 20:40:31 +0700

    base implementation di

--- app/src/main/java/com/prilepskiy/realmtestapp/MainActivity.kt
+++ app/src/main/java/com/prilepskiy/realmtestapp/MainActivity.kt
@@ -3,15 +3,34 @@ package com.prilepskiy.realmtestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prilepskiy.realmtestapp.ui.theme.RealmTestAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +41,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Container()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Container() {
 
-@Preview(showBackground = true)
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column() {
+            LoginContent()
+            PasswordContent()
+        }
+
+
+    }
+
+
+
+}
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    RealmTestAppTheme {
-        Greeting("Android")
+fun LoginContent(){
+    Column(Modifier.padding(25.dp)) {
+        Text(text = "Login/Email")
+        val textState = remember { mutableStateOf(TextFieldValue()) }
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email
+            )
+        )
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordContent(){
+    Column(Modifier.padding(25.dp)) {
+        val password = remember { mutableStateOf(TextFieldValue()) }
+        Text(text = "Password")
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            )
+        )
+
+        var result = "incorrect"
+        var textColor = Color.Red
+        if (password.value.text == "secret"){
+            result = "correct"
+            textColor = Color.Green
+        }
+
+        Text(
+            text = "Password : $result",
+            fontSize = 22.sp,
+            color = textColor,
+            fontFamily = FontFamily.SansSerif,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.padding(top = 25.dp)
+        )
     }
 }
